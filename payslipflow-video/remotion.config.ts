bd7fdf23be29e4091a05994b441014884135f5a6
Ibiene West <151536890+WestIbiene@users.noreply.github.com>
2026-08-30 19:54:19 +0100

    Delete payslipflow-video directory

--- payslipflow-video/remotion.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from '@remotion/cli/config';
-
-// Higher CRF = smaller file, lower quality. 18 is visually near-lossless
-// and totally fine for a YouTube upload.
-Config.setVideoImageFormat('jpeg');
-Config.setCodec('h264');
-Config.setCrf(18);
-Config.setPixelFormat('yuv420p');
